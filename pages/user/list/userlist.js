b628ada7b467a0c602c6f607656c0bc1eebea71d
马凌博 <malingbo@newjincin.com>
2019-03-14 12:51:31 +0800

    add onreachbottom and enablepulldownrefresh function

--- pages/user/list/userlist.js
+++ pages/user/list/userlist.js
@@ -5,88 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-    "userlist":[
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-      {
-        "name":"rambo",
-        "age":"30",
-        "image":"/images/tabbar/shop1.png",
-        "address":"北京朝阳区"
-      },
-    ]
-
+    userlist: []
   },
-
+  initdata: function () {
+    this.data.userlist = [];
+    this.getpage();
+  },
+  getpage: function () {
+    var currentuserlist = this.data.userlist;
+    for (var userindex = 0; userindex < 10; userindex++) {
+      var demodata = {
+        "name": "rambo",
+        "age": "30",
+        "image": "/images/tabbar/shop1.png",
+        "address": "北京朝阳区"
+      };
+      currentuserlist.push(demodata);
+    }
+    this.setData({
+      userlist: currentuserlist
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.initdata();
   },
 
   /**
@@ -121,14 +65,23 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    wx.showNavigationBarLoading() //在标题栏中显示加载
 
+    //模拟加载
+    setTimeout(function () {
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1500);
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.loading=true;
+    this.getpage();
+    this.loading=false;
   },
 
   /**
